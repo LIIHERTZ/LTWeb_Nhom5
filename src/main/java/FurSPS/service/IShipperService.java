e6fdac5185c2fc8f225ff235634c1403b364c15a
Nguyen Dinh Hong Phuc <phuc11012904@gmail.com>
2024-11-27 09:57:00 +0700

    hoan thanh shipper voi template co ban

---
+++ src/main/java/FurSPS/service/IShipperService.java
@@ -0,0 +1,19 @@
+package FurSPS.service;
+
+import java.util.List;
+
+import FurSPS.models.UserModel;
+
+public interface IShipperService {
+	List<UserModel> findAllShipper();
+
+	void updateShipper(UserModel model);
+
+	UserModel findOne(int id);
+
+	void deleteShipper(int id);
+
+	void insertShipper(UserModel model);
+
+	int createShipperID();
+}
